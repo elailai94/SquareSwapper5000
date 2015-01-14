cbe67b5529f514dfe76f82c62dcb2437eec9c3b4
Elisha Lai <elisha.lai@outlook.com>
2015-01-13 23:41:21 -0500

    Create basicsquare.h
    Added basicsquare.h to current directory

---
+++ basicsquare.h
@@ -0,0 +1,23 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with BasicSquare objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// BasicSquare module (basicsquare.h)
+
+#ifndef __BASICSQUARE__
+#define __BASICSQUARE__
+
+#include "square.h"
+
+// Object definition
+class BasicSquare: public Square {
+public:
+   BasicSquare();  // Constructor
+   ~BasicSquare(); // Destructor
+};
+
+#endif
