49e71aa5633b3ed068cda21cdee694087cb17636
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:10:41 -0500

    Create lateralsquare.cc
    Added lateralsquare.cc to current directory

---
+++ lateralsquare.cc
@@ -0,0 +1,21 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with LateralSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// LateralSquare module (lateralsquare.cc)
+
+#include "lateralsquare.h"
+
+using namespace std; // Provides direct access to std
+
+// See interface (header file).
+LateralSquare::LateralSquare()
+   : Square('h') {} // Constructor
+
+// See interface (header file).
+LateralSquare::~LateralSquare() {} // Destructor
