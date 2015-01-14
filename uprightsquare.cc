9679da6f7c13e58c0b25886d6e02aa1041d80534
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:31:04 -0500

    Create uprightsquare.cc
    Added uprightsquare.cc to current directory

---
+++ uprightsquare.cc
@@ -0,0 +1,21 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with UprightSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// UprightSquare module (uprightsquare.cc)
+
+#include "uprightsquare.h"
+
+using namespace std; // Provides direct access to std
+
+// See interface (header file).
+UprightSquare::UprightSquare()
+    : Square('v') {} // Constructor
+
+// See interface (header file).
+UprightSquare::~UprightSquare() {} // Destructor
