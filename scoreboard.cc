793db8a698b226633c517d38faaf6ebc6f723a71
Elisha Lai <elisha.lai@outlook.com>
2015-01-13 23:20:25 -0500

    Update scoreboard.cc

--- scoreboard.cc
+++ scoreboard.cc
@@ -20,71 +20,71 @@ Scoreboard::Scoreboard(int level)
    : initScore(0), curScore(0), level(level) {} // Constructor
 
 // See interface (header file).
-unsigned long ScoreBoard::getInitScore() {
-   return initScore;
+unsigned long Scoreboard::getInitScore() {
+    return initScore;
 } // getInitScore
 
 // See interface (header file).
-unsigned long ScoreBoard::getScore() {
-	return curScore;
+unsigned long Scoreboard::getScore() {
+   return curScore;
 } // getScore
 
 // See interface (header file).
-void ScoreBoard::addScore(const int s) {
-	curScore = curScore + s;
+void Scoreboard::addScore(const int s) {
+   curScore = curScore + s;
 } // addScore
 
 // See interface (header file).
-void ScoreBoard::rollbackScore() {
-	curScore = initScore;
+void Scoreboard::rollbackScore() {
+   curScore = initScore;
 } // rollbackScore
 
 // See interface (header file).
-int ScoreBoard::getLevel() {
-	return level;
+int Scoreboard::getLevel() {
+   return level;
 } // getLevel
 
 // See interface (header file).
-void ScoreBoard::incLevel(Board *b) {
-	if (level < MAXLEVEL) {
-		level++;
-		setInitScore(curScore);
-		b->restart();
-	} else {
-		cout << *b << endl;
-	}
+void Scoreboard::incLevel(Board *b) {
+   if (level < MAXLEVEL) {
+      level++;
+      setInitScore(curScore);
+      b->restart();
+    } else {
+      cout << *b << endl;
+    } // if
 } // incLevel
 
 // See interface (header file).
-bool ScoreBoard::isMaxLevel() {
-	return level == MAXLEVEL;
+bool Scoreboard::isMaxLevel() {
+   return level == MAXLEVEL;
 } // isMaxLevel
 
 // See interface (header file).
-void ScoreBoard::decLevel(Board *b) {
-	if (level > 0) {
-		level--;
-		setInitScore(curScore);
-		b->restart();
-	} else {
-		cout << *b << endl;
-	} // if
+void Scoreboard::decLevel(Board *b) {
+   if (level > 0) {
+      level--;
+      setInitScore(curScore);
+      b->restart();
+   } else {
+      cout << *b << endl;
+   } // if
 } // decLevel
 
 // See interface (header file).
-void ScoreBoard::setInitScore(const unsigned long score) {
-	initScore = score;
+void Scoreboard::setInitScore(const unsigned long score) {
+   initScore = score;
 } // setInitScore
 
 // See interface (header file).
-bool ScoreBoard::isLevelComplete(Board *b) {
-	if (level == 0) {
-		return (curScore - initScore) >= 200;
-	} else if (level == 1) {
-		return (curScore - initScore) >= 300;
-	} else if (level == 2) {
-		return ((curScore - initScore) >= 500) && b->unlocked();
-	} // if
-	
-	return false;
+bool Scoreboard::isLevelComplete(Board *b) {
+   if (level == 0) {
+      return (curScore - initScore) >= 200;
+   } else if (level == 1) {
+      return (curScore - initScore) >= 300;
+   } else if (level == 2) {
+      return ((curScore - initScore) >= 500) && b->unlocked();
+   } // if
+
+   return false;
 } // isLevelComplete
