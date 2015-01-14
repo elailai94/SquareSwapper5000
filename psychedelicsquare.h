c3102c5c41b30e3b915e1a9e6352fa558e05df41
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:21:40 -0500

    Create psychedelicsquare.h
    Added psychedelicsquare.h to current directory

---
+++ psychedelicsquare.h
@@ -0,0 +1,24 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with PsychedelicSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// PsychedelicSquare module (psychedelicsquare.h)
+
+#ifndef __PSYCHEDELICSQUARE_H__
+#define __PSYCHEDELICSQUARE_H__
+
+#include "square.h"
+
+// Object definition
+class PsychedelicSquare: public Square {
+public:
+	PsychedelicSquare();  // Constructor
+	~PsychedelicSquare(); // Destructor
+};
+
+#endif
