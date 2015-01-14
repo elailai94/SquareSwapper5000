acc43aa7860eb2d849873059d89a17308493a91b
Elisha Lai <elisha.lai@outlook.com>
2015-01-13 23:46:08 -0500

    Create basicsquare.cc
    Added basicsquare.cc to current directory

---
+++ basicsquare.cc
@@ -0,0 +1,20 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with BasicSquare objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// BasicSquare module (basicsquare.cc)
+
+#include "basicsquare.h"
+
+using namespace std; // Provides direct access to std
+
+// See interface (header file).
+BasicSquare::BasicSquare()
+   : Square('_') {} // Constructor
+
+// See interface (header file).
+BasicSquare::~BasicSquare() {} // Destructor
