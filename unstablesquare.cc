4a461bb9204a8a0d6430a8bb3c7897c1437764d1
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:28:21 -0500

    Create unstablesquare.cc
    Added unstablesquare.cc to current directory

---
+++ unstablesquare.cc
@@ -0,0 +1,21 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with UnstableSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// UnstableSquare module (unstablesquare.cc)
+
+#include "unstablesquare.h"
+
+using namespace std; // Provides direct acccess to std
+
+// See interface (header file).
+UnstableSquare::UnstableSquare()
+   : Square('b') {} // Constructor
+
+// See interface (header file).
+UnstableSquare::~UnstableSquare() {} // Destructor
