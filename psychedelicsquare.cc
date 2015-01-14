38da210c47721bc1eae5d413044914e9d95dfdec
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:25:57 -0500

    Create psychedelicsquare.cc
    Added psychedelicsquare.cc to current directory

---
+++ psychedelicsquare.cc
@@ -0,0 +1,21 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with PsychedelicSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// PsychedelicSquare module (psychedelicsquare.cc)
+
+#include "psychedelicsquare.h"
+
+using namespace std; // Provides direct access to std
+
+// See interface (header file).
+PsychedelicSquare::PsychedelicSquare()
+   : Square('p') {} // Constructor
+
+// See interface (header file).
+PsychedelicSquare::~PsychedelicSquare() {} // Destructor
