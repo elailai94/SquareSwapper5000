d55e5e273d1287a0e3cf148d0aad422cabb79611
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:27:16 -0500

    Create unstablesquare.h
    Added unstablesquare.h to current directory

---
+++ unstablesquare.h
@@ -0,0 +1,24 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with UnstableSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// UnstableSquare module (unstablesquare.h)
+
+#ifndef __UNSTABLESQUARE_H__
+#define __UNSTABLESQUARE_H__
+
+#include "square.h"
+
+// Object definition
+class UnstableSquare: public Square {
+public:
+	UnstableSquare();  // Constructor
+	~UnstableSquare(); // Destructor	
+};
+
+#endif
