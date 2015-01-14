32dc8cd5411a74a7a8792c9f23ed7957f9bd4a06
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:30:00 -0500

    Create uprightsquare.h
    Added uprightsquare.h to current directory

---
+++ uprightsquare.h
@@ -0,0 +1,24 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with UprightSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// UprightSquare module (uprightsquare.h)
+
+#ifndef __UPRIGHTSQUARE_H__
+#define __UPRIGHTSQUARE_H__
+
+#include "square.h"
+
+// Object definition
+class UprightSquare: public Square {
+public:
+	UprightSquare();  // Constructor
+	~UprightSquare(); // Destructor
+};
+
+#endif
