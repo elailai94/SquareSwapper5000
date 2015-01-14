67552b80b75c49dc5ba07f05a0cbd2ae5e0190a2
Elisha Lai <elisha.lai@outlook.com>
2015-01-13 23:54:48 -0500

    Create lateralsquare.h
    Added lateralsquare.h to current directory

---
+++ lateralsquare.h
@@ -0,0 +1,24 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with LateralSquare
+// objects
+// @version: 1.0 17/11/2014
+//==============================================================================
+
+// LateralSquare module (lateralsquare.h)
+
+#ifndef __LATERALSQUARE_H__
+#define __LATERALSQUARE_H__
+
+#include "square.h"
+
+// Object definition
+class LateralSquare: public Square {
+public:
+	LateralSquare();  // Constructor
+	~LateralSquare(); // Destructor
+};
+
+#endif
