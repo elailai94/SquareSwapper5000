faaf47b991f5b6557d430abc0ac1c719564df99e
Elisha Lai <elisha.lai@outlook.com>
2015-01-14 00:57:39 -0500

    Create restart.cc
    Added restart.cc to current directory

---
+++ restart.cc
@@ -0,0 +1,28 @@
+//==============================================================================
+// SquareSwapper 5000
+//
+// @author: Elisha Lai & Laxman Sooriyathas
+// @description: Module for providing functions to work with Restart objects
+// @version: 1.0 21/11/2014
+//==============================================================================
+
+// Restart module (restart.cc)
+
+#include "restart.h"
+#include "board.h"
+
+using namespace std; // Provides direct access to std
+
+// See interface (header file).
+Restart::Restart(Board *board)
+   : Command(board) {} // Constructor
+
+// See interface (header file).
+Restart::~Restart() {} // Destructor
+
+// See interface (header file).
+void Restart::run() {
+   if (getBoard() != 0) { // theBoard isn't NULL?
+      getBoard()->restart();
+   } // if
+} // run
